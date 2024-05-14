485bf8e1b13e9a6aa1f9f5a5a030d490ab3417bb
Nikola Stancic <88898574+Nikoraii@users.noreply.github.com>
2024-05-15 01:01:37 +0200

    Added new project

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -1,3 +1,5 @@
+import { FaGithub } from "react-icons/fa";
+
 export default function Projects() {
     return (
         <div className="section" id="projects">
@@ -14,7 +16,7 @@ export default function Projects() {
                     <div className="project-description">
                         <div className="project-title">
                             <h4>STARRAIDLE:</h4>
-                            <img src="https://skillicons.dev/icons?i=vue,tailwind,nodejs,express" alt="HTML & CSS" />
+                            <img src="https://skillicons.dev/icons?i=vue,tailwind,nodejs,express" alt="Vue, Tailwind CSS, Node.js and Express.js icons" />
                         </div>
                         <div className="project-info">
                             <p>
@@ -32,36 +34,38 @@ export default function Projects() {
                     </div>
                 </div>
 
-                <div className="more-info">
+                {/* <div className="more-info">
                     More projects currently in development...
-                </div>
+                </div> */}
 
-                {/* TO ADD LATER */}
-                {/* <div className="project">
+                <div className="project">
                     <div className="project-picture">
-                        <a href="https://honkai-starraidle-frontend.vercel.app/" rel="noreferrer" target="_blank">
-                            <img src="assets/starraidle-guessed.png" alt="Starraidle screenshot" />
+                        <a href="https://dod-projects.vercel.app/" rel="noreferrer" target="_blank">
+                            <img src="assets/dod-projects.png" alt="DoD Projects screenshot" />
                         </a>
                     </div>
                     <div className="project-description">
                         <div className="project-title">
-                            <h4>STARRAIDLE:</h4>
-                            <img src="https://skillicons.dev/icons?i=vue,tailwind,nodejs,express" alt="HTML & CSS" />
+                            <h4>DoD Projects:</h4>
+                            <img src="https://skillicons.dev/icons?i=nextjs,react,ts,css" alt="Next.js, React and CSS icons" />
                         </div>
                         <div className="project-info">
                             <p>
-                                Daily character guessing game using characters from Honkai Star Rail with thousands of daily users.
+                                List of all public Department of defense projects.
                             </p>
                             <p>
-                                Each day the game resets with a new character to guess, same for everyone.
-                                Utilizing daily streak users are encouraged to play every day.
+                                Complete list of projects with sorting and filtering options for DoD Project. For every project additional data is
+                                displayed on their individual page. Data <a href="https://code.mil/code.json" target="_blank" rel="noreferrer">used</a>.
                             </p>
                             <div className="project-link">
-                                <a href="https://honkai-starraidle-frontend.vercel.app/" rel="noreferrer" target="_blank">starraidle.com</a>
+                                <a href="https://dod-projects.vercel.app/" rel="noreferrer" target="_blank">dod-projects.vercel.app</a>
+                                <a href="https://github.com/Nikoraii/dod-projects" target="_blank" rel="noreferrer" className="git-link">
+                                    <FaGithub />
+                                </a>
                             </div>
                         </div>
                     </div>
-                </div> */}
+                </div>
             </div>
         </div>
     )
